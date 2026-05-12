fde30732f2f4fb9fd42669757dd8c20780d9d26f
CVRobo <ktaneja0910@gmail.com>
2026-05-12 01:27:21 -0400

    cleaned up main

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,12 @@
 import Game from "./Wolfie2D/Loop/Game";
-import MainMenu from "./master-blaster/Scenes/MainMenu";
 import { MBControls } from "./master-blaster/MBControls";
 import TitleCard from "./master-blaster/Scenes/TitleCard";
-import CastleLevel from "./master-blaster/Scenes/CastleLevel";
-import WinterLevel from "./master-blaster/Scenes/WinterLevel";
 
 // The main function is your entrypoint into Wolfie2D. Specify your first scene and any options here.
 (function main(){
 
     // Set up options for our game
-    let options = {
+    const options = {
         canvasSize: {x: 1200, y: 800},          // The size of the game
         clearColor: {r: 34, g: 32, b: 52},   // The color the game clears to
         inputs: [
@@ -17,13 +14,13 @@ import WinterLevel from "./master-blaster/Scenes/WinterLevel";
             {name: MBControls.MOVE_RIGHT,   keys: ["d"]},
             {name: MBControls.JUMP,         keys: ["w", "space"]},
             {name: MBControls.DOWN,         keys: ["s"]},
-            {name: MBControls.ATTACK,       keys: ["x"]},
+            {name: MBControls.ATTACK,       keys: ["x"]},// mostly unused
             {name: MBControls.TRANSFORM,    keys: ["e"]},
             {name: MBControls.CYCLE_FORM,   keys: ["f"]},
-            {name: MBControls.ATTACK_LEFT,  keys: ["arrowleft"]},
-            {name: MBControls.ATTACK_RIGHT, keys: ["arrowright"]},
-            {name: MBControls.ATTACK_UP,    keys: ["arrowup"]},
-            {name: MBControls.ATTACK_DOWN,  keys: ["arrowdown"]},
+            {name: MBControls.ATTACK_LEFT,  keys: ["arrowleft"]},//also mostly unused
+            {name: MBControls.ATTACK_RIGHT, keys: ["arrowright"]},//''
+            {name: MBControls.ATTACK_UP,    keys: ["arrowup"]},//''
+            {name: MBControls.ATTACK_DOWN,  keys: ["arrowdown"]},//''
             {name: MBControls.PAUSE, keys: ["escape"]},
             {name: MBControls.CONFIRM,  keys: ["enter"]},
             {name: MBControls.CROUCH,  keys: ["shift"]},
@@ -39,19 +36,6 @@ import WinterLevel from "./master-blaster/Scenes/WinterLevel";
     // Create a game with the options specified
     const game = new Game(options);
 
-    // Expose debugging functions to console
-    (window as any).getPlayerPos = () => {
-        const sceneManager = game.getSceneManager();
-        const scene = (sceneManager as any).currentScene;
-        if (scene && (scene as any).player) {
-            const pos = (scene as any).player.position;
-            console.log(`Player position: (${pos.x.toFixed(1)}, ${pos.y.toFixed(1)})`);
-            return { x: pos.x, y: pos.y };
-        } else {
-            console.log("No active scene with player found");
-            return null;
-        }
-    };
 
     (window as any).getPlayerInfo = () => {
         const sceneManager = game.getSceneManager();
